9f4ece47a1a94ebca4e51fc6e8e33c7397e46b20
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-07-14 04:28:25 +0100

    feat: added connect wallet modal

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 let categories = document.getElementById('category')
 
-function seeDetails () {
-    categories.innerHTML = 'See details';
+//function seeDetails () {
+  //  categories.innerHTML = 'See details';
+//}
+
+//categories.addEventListener("mouseenter", seeDetails);
+//categories.addEventListener("mouseout", LogoHoverRemove);
+
+
+let openModal = document.getElementById('openModal')
+let closeModal = document.getElementById('closeModal')
+let body = document.getElementsByTagName('body')[0]
+let modal = document.getElementById('modal')    
+
+function showModal() {
+    modal.classList.toggle('hidden');
+    body.classList.toggle('overflow-hidden')
 }
 
-categories.addEventListener("mouseenter", seeDetails);
-categories.addEventListener("mouseout", LogoHoverRemove);
-
-
-let connectWalletModal = document.getElementById('connectWalletModal')
+openModal.addEventListener("click", showModal);
+closeModal.addEventListener("click", showModal);
